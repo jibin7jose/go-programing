0b0953fc090b68f5a7ebbb34ec5f94ec13adfc95
jibin jose <jibinjose884@gmail.com>
2026-03-02 21:17:12 +0530

    first go programing

---
+++ hello.go
@@ -0,0 +1,31 @@
+/*
+-----------------------------------------
+File Name: hello.go
+Description: Basic Go program (Day 1)
+Author: Jibin Jose
+-----------------------------------------
+
+Running Commands:
+
+1. Open terminal in this folder
+2. Run:
+   go run hello.go
+
+Expected Output:
+Hello, World!
+Welcome to Go Programming
+-----------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+    // Printing first message
+    fmt.Println("Hello, World!")
+
+    // Printing second message
+    fmt.Println("Welcome to Go Programming")
+}
